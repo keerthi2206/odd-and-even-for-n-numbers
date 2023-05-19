2d6ed12b3bc0dd496506de89baaa8b091e0764ba
keerthi2206 <133849801+keerthi2206@users.noreply.github.com>
2023-05-19 12:08:22 +0530

    Add files via upload

---
+++ evenandodd.py
@@ -0,0 +1,12 @@
+print('even numbers are')
+n= int(input('enter your number: '))
+
+for i in range(0,n):
+    if i%2 == 0:
+        print(i)
+
+print('odd numbers are')
+
+for i in range(0,n):
+    if i%2 == 1:
+        print(i)
